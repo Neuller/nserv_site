2bf49f81babd99b059dd913c9d0da32868f4ac0b
Neuller César <neuller97@hotmail.com>
2021-08-26 20:13:50 -0300

    Update lixo

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Principal from '../pages/Principal.vue'
 import Empresa from '../pages/Empresa.vue'
-import Manutencao from '../pages/Manutencao.vue'
-import Vendas from '../pages/Vendas.vue'
-import Desenvolvimento from '../pages/Desenvolvimento.vue'
 
 Vue.use(VueRouter)
 
@@ -18,21 +15,6 @@ const routes = [
     path: '/empresa',
     name: 'empresa',
     component: () => import(/* webpackChunkName: "about" */ '../pages/Empresa.vue')
-  },
-  {
-    path: '/manutencao',
-    name: 'manutencao',
-    component: () => import(/* webpackChunkName: "about" */ '../pages/Manutencao.vue')
-  },
-  {
-    path: '/vendas',
-    name: 'vendas',
-    component: () => import(/* webpackChunkName: "about" */ '../pages/Vendas.vue')
-  },
-  {
-    path: '/desenvolvimento',
-    name: 'desenvolvimento',
-    component: () => import(/* webpackChunkName: "about" */ '../pages/Desenvolvimento.vue')
   }
 ]
 
