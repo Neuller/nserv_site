0bc64ac34e34a97f3c64bd77ffca6e6af443b951
Neuller César <neuller97@hotmail.com>
2021-08-26 21:58:16 -0300

    Update

--- vue.config.js
+++ vue.config.js
@@ -2,23 +2,10 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: "./",
   configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /\.(pdf)(\?.*)?$/,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                name: 'files/[name].[hash:8].[ext]'
-              }
-            }
-          ]
-        }
-      ]
-    }
+    plugins: [
+      new MyAwesomeWebpackPlugin()
+    ]
   }
 }
 
