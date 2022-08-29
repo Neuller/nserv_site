85d4bc49b09ec813ab9ed82b8dcc4dea59af53d9
Ncesar <neuller.silva@grupopardini.com.br>
2022-08-29 09:02:46 -0300

    Update Changes

--- src/components/vendas/AllInOne.vue
+++ src/components/vendas/AllInOne.vue
@@ -6,12 +6,12 @@
         <template v-slot:imagem>
           <img :src="'/vendas/computadores/all_in_one_compaq.png'" height="250"/>
         </template>
-        <template v-slot:titulo>ALL IN ONE COMPAQ</template>
+        <template v-slot:titulo>ALL IN ONE COMPAQ 23,8"</template>
         <template v-slot:preco>R$ • 3.106,8</template>
         <template v-slot:codigo>COD. 7822</template>
-        <template v-slot:desc1>PROCESSADOR INTEL CORE I3 7ª GE</template>
-        <template v-slot:desc2>4GB MEMORIA</template>
-        <template v-slot:desc3>500GB HD</template>
+        <template v-slot:desc1>PROCESSADOR INTEL CORE I3 7100U (7GE)</template>
+        <template v-slot:desc2>4GB DDR4 MEMORIA</template>
+        <template v-slot:desc3>500GB HD + 16GB OPTANE</template>
         <template v-slot:desc4>WINDOWS 10</template>
       </templateCard>
       
@@ -20,10 +20,10 @@
         <template v-slot:imagem>
           <img :src="'/vendas/computadores/all_in_one_positivo.png'" height="250"/>
         </template>
-        <template v-slot:titulo>ALL IN ONE POSITIVO</template>
+        <template v-slot:titulo>ALL IN ONE POSITIVO 18,5"</template>
         <template v-slot:preco>R$ • 1.818,7</template>
         <template v-slot:codigo>COD. 8922</template>
-        <template v-slot:desc1>PROCESSADOR INTEL CORE I3</template>
+        <template v-slot:desc1>PROCESSADOR INTEL CORE I3 3110M (3GE)</template>
         <template v-slot:desc2>6GB MEMORIA</template>
         <template v-slot:desc3>120GB SSD</template>
         <template v-slot:desc4>WINDOWS 10</template>
@@ -34,10 +34,10 @@
         <template v-slot:imagem>
           <img :src="'/vendas/computadores/all_in_one_thinkcentre.png'" height="250"/>
         </template>
-        <template v-slot:titulo>ALL IN ONE THINKCENTRE E73Z</template>
+        <template v-slot:titulo>ALL IN ONE THINKCENTRE E73Z 18,5"</template>
         <template v-slot:preco>R$ • 1.688,7</template>
         <template v-slot:codigo>COD. 8833</template>
-        <template v-slot:desc1>PROCESSADOR INTEL CORE I3</template>
+        <template v-slot:desc1>PROCESSADOR INTEL CORE I3 (4GE)</template>
         <template v-slot:desc2>4GB MEMORIA</template>
         <template v-slot:desc3>500GB HD</template>
         <template v-slot:desc4>WINDOWS 10</template>
